eaef9868b56279a08311d522349f9bcec3df8de0
Bruno <brunovigentas@gmail.com>
2020-07-19 19:16:28 -0300

    Ajuste location

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         value="This content will always face you."
         look-at="[gps-camera]"
         scale="120 120 120"
-        gps-entity-place="latitude: -26,9154; longitude: -49,1149;"
+        gps-entity-place="latitude: -26.9156302; longitude: -49.1143945;"
       ></a-text>
       <a-camera gps-camera rotation-reader> </a-camera>
     </a-scene>
